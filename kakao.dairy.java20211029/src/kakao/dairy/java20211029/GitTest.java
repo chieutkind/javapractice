612631766825039ff05e96465be918c1ed626664
tjoeun <tjoeun@DESKTOP-GD76PAT>
2021-10-29 10:59:42 +0900

    test

---
+++ kakao.dairy.java20211029/src/kakao/dairy/java20211029/GitTest.java
@@ -0,0 +1,10 @@
+package kakao.dairy.java20211029;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("Testing");
+
+	}
+
+}
---
+++ kakao.dairy.java20211101/src/kakao/dairy/java20211101/GitTest.java
@@ -0,0 +1,10 @@
+package kakao.dairy.java20211101;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("Testing");
+
+	}
+
+}
